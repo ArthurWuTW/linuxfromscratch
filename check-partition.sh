6e1444b1d7960b8b26da79370406586d3cb7dbf3
root <root@2001-b011-e600-1329-e000-b92d-8d73-8cef.dynamic-ip6.hinet.net>
2023-06-22 00:01:27 -0400

    add env and check-partition.sh

---
+++ check-partition.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+lsblk
+df -kh
+cat /etc/fstab
--- run.sh
+++ run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-echo "Run version-check.sh..."
-./version-check.sh
+source env.sh
 
-echo "Install host pkg..."
-./install_host_pkg.sh
+if [ ${VERSION_CHECK} == 1 ];
+then
+  echo "Run version-check.sh..."
+  ./version-check.sh
 
-echo "Run version-check.sh..."
-./version-check.sh
+fi
+
+if [ ${INSTALL_HOST_PKG} == 1 ];
+then
+  echo "Install host pkg..."
+  ./install_host_pkg.sh
+fi
+
+if [ ${CHECK_PARTITION} == 1 ];
+then
+  echo "Run check-partition.sh..."
+  ./check-partition.sh
+
+fi
+
+
