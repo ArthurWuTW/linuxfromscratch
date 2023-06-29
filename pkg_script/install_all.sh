10b1ba9e0e4ae7bf87c27732a278633cc0f6445e
root <root@localhost.localdomain>
2023-06-29 09:56:47 -0400

    create glibc symbolic link

---
+++ pkg_script/install_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./install_binutils.sh
+./install_gcc.sh
+./install_linux_headers.sh
+./install_glibc.sh
+./install_glibc_setup_symbolic_link.sh
---
+++ pkg_script/install_glibc_setup_symbolic_link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export LFS=/mnt/lfs
+
+case $(uname -m) in
+  x86_64) 
+    ln -sfv $LFS/lib/ld-linux-x86-64.so.2 $LFS/lib64
+    ln -sfv $LFS/lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+;;
+esac
