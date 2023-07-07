32aca5bdeec4e72ca0356a6710ab47e7ecfe9c27
root <root@localhost.localdomain>
2023-07-07 12:53:49 -0400

    Install texinfo

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_texinfo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export FILE=texinfo-7.0.2
+export FILE_XZ=${FILE}.tar.xz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+./configure --prefix=/usr
+
+make
+make install
