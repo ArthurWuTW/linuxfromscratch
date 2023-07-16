d5fdb72e53b79b5aaea263ce0d8a1354affef2b8
root <root@localhost.localdomain>
2023-07-16 01:53:24 -0400

    chroot install dejagnu

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_dejagnu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export FILE=dejagnu-1.6.3
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+mkdir -v build
+cd build
+
+../configure --prefix=/usr
+makeinfo --html --no-split -o doc/dejagnu.html ../doc/dejagnu.texi
+makeinfo --plaintext -o doc/dejagnu.txt ../doc/dejagnu.texi
+
+make install
+install -v -dm755 /usr/share/doc/dejagnu-1.6.3
+install -v -m644 doc/dejagnu.{html,txt} /usr/share/doc/dejagnu-1.6.3
+make check
