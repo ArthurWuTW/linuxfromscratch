5d231dbd0751a2a6ceed669dc31fbffcc6c93bce
root <root@localhost.localdomain>
2023-07-15 23:55:08 -0400

    install zlib

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_zlib.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export FILE=zlib-1.2.13
+export FILE_XZ=${FILE}.tar.xz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+./configure --prefix=/usr
+make
+make check
+rm -fv /usr/lib/libz.a
