9039d4670dd2f60ad9ba4c3d2815715d71f2779e
root <root@localhost.localdomain>
2023-07-16 02:23:41 -0400

    chroot install gmp

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_gmp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export FILE=gmp-6.2.1
+export FILE_XZ=${FILE}.tar.xz
+export SRC_DIR=/sources
+export CHROOT_PKG=${SRC_DIR}/chroot-pkg
+
+mkdir -p $CHROOT_PKG
+
+if [ ! -d ${CHROOT_PKG}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${CHROOT_PKG}/
+fi
+
+cd ${CHROOT_PKG}/${FILE}
+
+./configure --prefix=/usr \
+  --enable-cxx \
+  --disable-static \
+  --docdir=/usr/share/doc/gmp-6.2.1
+make
+make html
+make check 2>&1 | tee gmp-check-log
+awk '/# PASS:/{total+=$3} ; END{print total}' gmp-check-log
+make install
+make install-html
