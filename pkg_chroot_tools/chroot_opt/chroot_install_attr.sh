56396bc6aeace0712e612e5519e090ecf93199f0
root <root@localhost.localdomain>
2023-07-16 02:35:48 -0400

    chroot install attr

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_attr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export FILE=attr-2.5.1
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+./configure --prefix=/usr \
+  --disable-static \
+  --sysconfdir=/etc \
+  --docdir=/usr/share/doc/attr-2.5.1
+
+make
+make check
+make install
