6c6960ebea565eb85200adcc3d03d353de5f0e4f
root <root@localhost.localdomain>
2023-07-15 23:51:03 -0400

    configure dynamic loader

---
+++ pkg_chroot_tools/chroot_opt/chroot_configure_dynamic_loader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat > /etc/ld.so.conf << "EOF"
+# Begin /etc/ld.so.conf
+/usr/local/lib
+/opt/lib
+EOF
+
+cat >> /etc/ld.so.conf << "EOF"
+# Add an include directory
+include /etc/ld.so.conf.d/*.conf
+EOF
+mkdir -pv /etc/ld.so.conf.d
+
