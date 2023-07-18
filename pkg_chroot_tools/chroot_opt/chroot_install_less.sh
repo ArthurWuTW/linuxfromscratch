6d4feb570dc85064abc3272479f00b53227993d9
root <root@localhost.localdomain>
2023-07-18 10:14:30 -0400

    chroot install less

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_less.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export FILE=less-608
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+./configure --prefix=/usr --sysconfdir=/etc
+make
+make install
