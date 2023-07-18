14f1809aa9e5e22eb399b79c47805ddb79da51e0
root <root@localhost.localdomain>
2023-07-18 09:49:55 -0400

    install grep

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_grep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export FILE=grep-3.8
+export FILE_XZ=${FILE}.tar.xz
+export SRC_DIR=/sources
+export CHROOT_PKG=${SRC_DIR}/chroot-pkg
+
+mkdir -p $CHROOT_PKG
+
+if [ ! -d ${CHROOT_PKG}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${CHROOT_PKG}/
+fi
+
+cd ${CHROOT_PKG}/${FILE}
+
+sed -i "s/echo/#echo/" src/egrep.sh
+./configure --prefix=/usr
+make
+make check
+make install
