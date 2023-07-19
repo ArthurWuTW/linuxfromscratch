1e34a5ff26b130a6731c9f7dc0caec6b6d8c85f6
root <root@localhost.localdomain>
2023-07-19 09:29:48 -0400

    chroot install openssl

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_openssl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export FILE=openssl-3.0.8
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+./config --prefix=/usr \
+  --openssldir=/etc/ssl \
+  --libdir=lib \
+  shared \
+  zlib-dynamic
+
+make
+make test
+sed -i '/INSTALL_LIBS/s/libcrypto.a libssl.a//' Makefile
+make MANSUFFIX=ssl install
+mv -v /usr/share/doc/openssl /usr/share/doc/openssl-3.0.8
+cp -vfr doc/* /usr/share/doc/openssl-3.0.8
