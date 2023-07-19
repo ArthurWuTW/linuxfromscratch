a2c99390fa6083e0da74551a7e01ab1c1a7e2cab
root <root@localhost.localdomain>
2023-07-19 11:00:51 -0400

    chroot install ninja

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_ninja.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export FILE=ninja-1.11.1
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+export NINJAJOBS=4
+
+sed -i '/int Guess/a \
+  int   j = 0;\
+  char* jobs = getenv( "NINJAJOBS" );\
+  if ( jobs != NULL ) j = atoi( jobs );\
+  if ( j > 0 ) return j;\
+' src/ninja.cc
+
+python3 configure.py --bootstrap
+./ninja ninja_test
+./ninja_test --gtest_filter=-SubprocessTest.SetWithLots
+
+install -vm755 ninja /usr/bin/
+install -vDm644 misc/bash-completion /usr/share/bash-completion/completions/ninja
+install -vDm644 misc/zsh-completion  /usr/share/zsh/site-functions/_ninja
