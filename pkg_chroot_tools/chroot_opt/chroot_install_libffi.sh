57a9f9559e90ae2861ff9fcebc7b21da80dcbb58
root <root@localhost.localdomain>
2023-07-19 10:01:11 -0400

    chroot isntall libffi

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_libffi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export FILE=libffi-3.4.4
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+./configure --prefix=/usr \
+  --disable-static \
+  --with-gcc-arch=native
+
+make
+make check
+make install
