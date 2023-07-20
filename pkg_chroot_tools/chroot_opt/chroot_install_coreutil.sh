74e2031c1cfa6d569799d7c769671e77b2a24dfb
root <root@localhost.localdomain>
2023-07-20 11:23:13 -0400

    chroot install coreutils

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_coreutil.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export FILE=coreutils-9.1
+export FILE_XZ=${FILE}.tar.xz
+export SRC_DIR=/sources
+export CHROOT_PKG=${SRC_DIR}/chroot-pkg
+
+mkdir -p $CHROOT_PKG
+
+if [ ! -d ${CHROOT_PKG}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${CHROOT_PKG}/
+fi
+
+cd ${CHROOT_PKG}/${FILE}
+
+patch -Np1 -i ../../coreutils-9.1-i18n-1.patch
+autoreconf -fiv
+FORCE_UNSAFE_CONFIGURE=1 ./configure \
+  --prefix=/usr \
+  --enable-no-install-program=kill,uptime
+make
+make install
+mv -v /usr/bin/chroot /usr/sbin
+mv -v /usr/share/man/man1/chroot.1 /usr/share/man/man8/chroot.8
+sed -i 's/"1"/"8"/' /usr/share/man/man8/chroot.8
