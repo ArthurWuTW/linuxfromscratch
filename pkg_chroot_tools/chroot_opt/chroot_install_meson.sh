51bd85864fb5070fc99030c67a7bdc0f69743bb1
root <root@localhost.localdomain>
2023-07-20 11:16:54 -0400

    chroot install meson

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_meson.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export FILE=meson-1.0.0
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+pip3 wheel -w dist --no-build-isolation --no-deps $PWD
+pip3 install --no-index --find-links dist meson
+install -vDm644 data/shell-completions/bash/meson /usr/share/bash-completion/completions/meson
+install -vDm644 data/shell-completions/zsh/_meson /usr/share/zsh/site-functions/_meson
