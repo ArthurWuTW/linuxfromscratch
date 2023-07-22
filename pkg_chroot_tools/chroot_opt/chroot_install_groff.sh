dafb7135bf44e4b80a46978e37cac3182464d4f8
root <root@localhost.localdomain>
2023-07-21 22:19:50 -0400

    chroot install groff

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_groff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export FILE=groff-1.22.4
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+
+PAGE=A4 ./configure --prefix=/usr
+make
+make install
