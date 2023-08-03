4e51acb01a50aef732bef090309f1477ba334a37
root <root@localhost.localdomain>
2023-08-03 00:22:14 -0400

    configure /etc/fstab

--- pkg_chroot_tools/chroot_opt/chroot_system_configuration.sh
+++ pkg_chroot_tools/chroot_opt/chroot_system_configuration.sh
@@ -138,3 +138,24 @@ cat > /etc/shells << "EOF"
 EOF
 
 cat /etc/shells
+
+cat > /etc/fstab << "EOF"
+# Begin /etc/fstab
+
+# file system  mount-point  type     options             dump  fsck
+#                                                              order
+
+/dev/sdb1     /boot         ext2    defaults            0     0
+/dev/sdb2     /             ext2    defaults            1     1
+
+proc           /proc        proc     nosuid,noexec,nodev 0     0
+sysfs          /sys         sysfs    nosuid,noexec,nodev 0     0
+devpts         /dev/pts     devpts   gid=5,mode=620      0     0
+tmpfs          /run         tmpfs    defaults            0     0
+devtmpfs       /dev         devtmpfs mode=0755,nosuid    0     0
+tmpfs          /dev/shm     tmpfs    nosuid,nodev        0     0
+
+# End /etc/fstab
+EOF
+
+cat /etc/fstab
