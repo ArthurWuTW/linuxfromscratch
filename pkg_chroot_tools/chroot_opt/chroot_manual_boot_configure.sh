2f6ec548ea2b0bad4a1dcf7bd52129c7439314cd
root <root@localhost.localdomain>
2023-08-07 10:28:19 -0400

    configure grub-install

---
+++ pkg_chroot_tools/chroot_opt/chroot_manual_boot_configure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
+  echo "We are chrooted!"
+else
+  printf '%s\n' "****** [Error]: NOT chroot lfs environment !!!" >&2; exit 1;
+fi
+
+echo "============================================================================"
+echo "Manual Step: >> grub-install <LFS_PARTITION> (ex: grub-install /dev/sdb2 --target i386-pc --force)"
+echo "<LFS_PARTITION>: see the filesystem below with command >> df -kh"
+echo "============================================================================"
+
+BOOT_DF=$(df -kh | grep boot)
+if [ -z "$BOOT_DF" ]; then
+  echo "Grub install in / partition --- "
+  df -kh | grep "/$"
+else
+  echo "Grub install in /boot partition --- "
+  df -kh | grep boot
+fi
