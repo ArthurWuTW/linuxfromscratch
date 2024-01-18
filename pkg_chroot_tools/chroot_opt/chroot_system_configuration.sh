207dcf9f448e775bf63027cec1a033334187a3ff
ArthurWuTW <36653598+ArthurWuTW@users.noreply.github.com>
2024-01-18 10:53:30 -0500

    Update chroot_system_configuration.sh

--- pkg_chroot_tools/chroot_opt/chroot_system_configuration.sh
+++ pkg_chroot_tools/chroot_opt/chroot_system_configuration.sh
@@ -57,18 +57,12 @@ l6:6:wait:/etc/rc.d/init.d/rc 6
 ca:12345:ctrlaltdel:/sbin/shutdown -t1 -a -r now
 su:S06:once:/sbin/sulogin
 s1:1:respawn:/sbin/sulogin
-1:2345:respawn:/sbin/agetty
-2:2345:respawn:/sbin/agetty
-3:2345:respawn:/sbin/agetty
-4:2345:respawn:/sbin/agetty
-5:2345:respawn:/sbin/agetty
-6:2345:respawn:/sbin/agetty
---noclear tty1 9600
-tty2 9600
-tty3 9600
-tty4 9600
-tty5 9600
-tty6 9600
+1:2345:respawn:/sbin/agetty --noclear tty1 9600
+2:2345:respawn:/sbin/agetty tty2 9600
+3:2345:respawn:/sbin/agetty tty3 9600
+4:2345:respawn:/sbin/agetty tty4 9600
+5:2345:respawn:/sbin/agetty tty5 9600
+6:2345:respawn:/sbin/agetty tty6 9600
 # End /etc/inittab
 EOF
 
