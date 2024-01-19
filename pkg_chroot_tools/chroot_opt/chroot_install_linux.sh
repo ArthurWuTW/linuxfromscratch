1b978df7e2df127a1497b560df805d13f1aabcc2
ArthurWuTW <ArthurWuTW>
2024-01-19 11:06:08 -0500

    fix glibc compile hanging and add linux menuconfig

--- pkg_chroot_tools/chroot_opt/chroot_install_linux.sh
+++ pkg_chroot_tools/chroot_opt/chroot_install_linux.sh
@@ -15,7 +15,33 @@ fi
 cd ${CHROOT_PKG}/${FILE}
 
 make mrproper
-make defconfig
+make menuconfig
+
+# [*] Device Drivers, Generic Driver Options, Maintain a devtmpfs filesystem to mount at /dev
+# [*] Device Drivers, Network device support, Ethernet Driver support, AMD PCnet32 PCI support
+# [*] Device Drivers, Fusion MPT device support (select the three drivers for SPI, FC, SAS)
+# [*] Device Drivers, SCSI device support, SCSI low-level drivers
+# [*] File Systems, Ext3 Journaling file system support
+
+# [*] Processor type and features, Build a relocatable kernel
+# [*] Processor type and features, Randomize the address of the kernel image
+# [ ] General setup, Compile the kernel with warnings as errors
+# [ ] General setup, Enable kernel headers through /sys/kernel/kheaders.tar.xz
+# [*] General architecture-dependent options, Stack Protector buffer overflow detection
+# [*] General architecture-dependent options, Strong Stack Protector
+# [*] Device Drivers, Graphics support, Frame buffer Devices, Support for frame buffer devices
+# [*] Device Drivers, Graphics support, Console display driver support, Framebuffer Console support
+# [ ] Device Drivers, Generic Driver Options, Support for uevent helper
+# [*] Device Drivers, Generic Driver Options, Maintain a devtmpfs filesystem to mount at /dev
+# [*] Device Drivers, Generic Driver Options, Automount devtmpfs at /dev, after the kernel mounted the rootfs
+# [*] Processor type and features, Support x2apic
+# [*] Device Drivers, PCI Support
+# [*] Device Drivers, PCI Support, Message Signaled Interrupts (MSI and MSI-X)
+# [*] Device Drivers, IOMMU Hardware Support
+# [*] Device Drivers, IOMMU Hardware Support, Support for Interrupt Remapping
+
+
+
 make -j4
 make modules_install
 cp -iv arch/x86/boot/bzImage /boot/vmlinuz-6.1.11-lfs-11.3
