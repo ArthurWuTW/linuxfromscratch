1441153dc332d85f7a2933b4d73dd254c7ad3326
ArthurWuTW <ArthurWuTW>
2024-01-23 10:27:35 -0500

    chroot install openssh

---
+++ pkg_chroot_tools/chroot_opt/chroot_install_openssh.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export FILE=openssh-9.4p1
+export FILE_XZ=${FILE}.tar.gz
+export SRC_DIR=/sources
+export CURR_USER=$(whoami)
+
+if [ ! -d ${SRC_DIR}/${FILE} ];
+then
+  tar xvf ${SRC_DIR}/${FILE_XZ} -C ${SRC_DIR}/
+fi
+
+cd $SRC_DIR/$FILE
+
+install -v -g sys -m700 -d /var/lib/sshd
+groupadd -g 50 sshd
+useradd  -c 'sshd PrivSep' \
+         -d /var/lib/sshd  \
+         -g sshd           \
+         -s /bin/false     \
+         -u 50 sshd
+./configure --prefix=/usr                            \
+            --sysconfdir=/etc/ssh                    \
+            --with-privsep-path=/var/lib/sshd        \
+            --with-default-path=/usr/bin             \
+            --with-superuser-path=/usr/sbin:/usr/bin \
+            --with-pid-dir=/run
+
+make
+make install
+install -v -m755    contrib/ssh-copy-id /usr/bin
+install -v -m644    contrib/ssh-copy-id.1 \
+                    /usr/share/man/man1
+install -v -m755 -d /usr/share/doc/openssh-9.4p1
+install -v -m644    INSTALL LICENCE OVERVIEW README* \
+                    /usr/share/doc/openssh-9.4p1
