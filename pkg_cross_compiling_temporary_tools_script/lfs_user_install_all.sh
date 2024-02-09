38a7220c4c154b0b8678688aa09a5a386fa7143e
root <root@localhost.localdomain>
2024-02-09 10:10:17 -0500

    Fix script

--- pkg_cross_compiling_temporary_tools_script/lfs_user_install_all.sh
+++ pkg_cross_compiling_temporary_tools_script/lfs_user_install_all.sh
@@ -5,11 +5,11 @@ CURR_USER=$(whoami)
 if [ ! "${CURR_USER}" == "lfs" ];
 then
   echo "Current user is ${CURR_USER}" && { printf '%s\n' "****** [Error]: NOT as lfs user! Please use command>>> su lfs " >&2; exit 1; }
-  
-#  echo "Run change_mnt_lfs_owner_to_lfs_user.sh..."
-#  ./change_mnt_lfs_owner_to_lfs_user.sh
 
 else
+ 
+#  echo "Run change_mnt_lfs_owner_to_lfs_user.sh..."
+#  ./change_mnt_lfs_owner_to_lfs_user.sh
 
   echo "Run install_m4.sh..."
   ./install_m4.sh
